1b12d5c27866c7127142c24e9d286a26657543a2
Jay <coldextremely@yahoo.com>
2017-11-08 21:45:36 -0700

    Reformatted the code

--- OrderBook.java
+++ OrderBook.java
@@ -4,80 +4,66 @@ import java.awt.Dimension;
 import java.awt.Point;
 import java.util.ArrayList;
 
+import GenCol.entity;
 import GenCol.Queue;
+import model.modeling.content;
+import model.modeling.message;
 import view.modeling.ViewableAtomic;
 import view.modeling.ViewableComponent;
 import view.modeling.ViewableDigraph;
 
 public class OrderBook extends ViewableAtomic {
 
-	protected Queue matchQ;
-	protected int id;
-	protected float numBitcoin;
-	protected float cash;
-	protected boolean match;
-	
-	public OrderBook() {
-		this("OrderBook",0,0,0,0,0,false);
-	}
-	
-		public OrderBook(
-				String name,
-				int idKey,
-				int idSell,
-				int idBuy,
-				float numBitcoin, 
-				float cash,
-				boolean match) 
-		{
-			super(name);
-			matchQ = new Queue();
-			addInport("InOrders");
-			addOutport("OutTransactions");
-			this.idKey = idKey;
-			this.idSell = idSell;
-			this.idBuy = idBuy;
-			this.numBitcoin = numBitcoin;
-			this.cash = cash;			
-			this.match = match;
-		}
-	
-		public void initialize() {
-			super.initialize();
-			matchQ = new Queue();
-			sigma = INFINITY;
-			phase = "passive";		
-		}
-		
-		public void deltext(double e, message x) {
-			Continue(e);
-			
-			if (phaseIs("passive")) {
-				for (int i = 0; i < x.getLength(); i++)
-					if (messageOnPort(x, "Orders", i)){
-						holdIn("mathcing", 100);
-					}
-			}
-		}
-		
-		public void deltint() {
-			passivate();			
-		}
-		
-		public message out() {
+    protected Queue matchQ;
+    protected boolean match;
 
-			message m = new message();
-					
-			content con = makeContent("Transactions", new entity("Transaction"));		
-			if (phaseIs ("matching")) {
-				m.add(con);	
-			}
-						
-			return m;		
-		}
-		
-		public void showState() {
-			super.showState();
-		}
-		
+    public OrderBook() {
+        this("OrderBook");
+    }
+
+    public OrderBook(String name) {
+        super(name);
+        matchQ = new Queue();
+        addInport("InOrders");
+        addOutport("OutTransactions");
+    }
+
+    public void initialize() {
+        super.initialize();
+        matchQ = new Queue();
+        sigma = INFINITY;
+        phase = "passive";
+    }
+
+    public void deltext(double e, message x) {
+        Continue(e);
+
+        if (phaseIs("passive")) {
+            for (int i = 0; i < x.getLength(); i++)
+                if (messageOnPort(x, "InOrders", i)) {
+                    holdIn("matching", 100);
+                }
+        }
+    }
+
+    public void deltint() {
+        passivate();
+    }
+
+    public message out() {
+
+        message m = new message();
+
+        content con = makeContent("OutTransactions", new entity("Transaction"));
+        if (phaseIs("matching")) {
+            m.add(con);
+        }
+
+        return m;
+    }
+
+    public void showState() {
+        super.showState();
+    }
+
 }
