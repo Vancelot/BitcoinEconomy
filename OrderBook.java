4d238dd220242b5a379638eae0c3f546b0dfb8f6
Jay T <32994441+JShadowGuardian@users.noreply.github.com>
2017-11-18 09:43:22 -0700

    Added processing of input messages

--- OrderBook.java
+++ OrderBook.java
@@ -23,9 +23,9 @@ public class OrderBook extends ViewableAtomic {
     protected Queue matchQ;
     protected boolean match;
 
-    private List<Order> buyList;
-    private List<Order> sellList;
-    
+    private ArrayList<Order> buyList;
+    private ArrayList<Order> sellList;
+
     public OrderBook() {
         this("OrderBook");
     }
@@ -33,6 +33,10 @@ public class OrderBook extends ViewableAtomic {
     public OrderBook(String name) {
         super(name);
         matchQ = new Queue();
+        
+        buyList = new ArrayList<Order>();
+        sellList = new ArrayList<Order>();
+        
         addInport("InOrders");
         addOutport("OutTransactions");
     }
@@ -47,10 +51,23 @@ public class OrderBook extends ViewableAtomic {
     public void deltext(double e, message x) {
         Continue(e);
 
+        // When a new order is issued from any agent, 
+        // the Order Book will first insert it to the buy/sell list as follows:
+        // - Buy orders with higher limit price bi are placed on top of the buy list
+        // - Sell orders with the lower limit price si  are placed on top of the sell list
+        // - If new orders have the same limit price, orders with older issue time are 
+        //     placed before the recent orders.
+
         if (phaseIs("passive")) {
             for (int i = 0; i < x.getLength(); i++)
                 if (messageOnPort(x, "InOrders", i)) {
-                    holdIn("matching", 100);
+                    OrderEntity message = (OrderEntity) x.getValOnPort("InOrders", i);
+                    Order order = message.getv();
+                    if (order.orderType == Order.OrderType.BUY) {
+                        buyList.add(order);
+                    } else if (order.orderType == Order.OrderType.SELL) {
+                        buyList.add(order);
+                    }
                 }
         }
     }
